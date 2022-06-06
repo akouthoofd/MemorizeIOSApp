a6a40270d75d6e9520fee9ba72055bec7f3f2e23
Alexander Kouthoofd <akouthoofd@salesforce.com>
2022-06-05 19:21:33 -0700

        Handle Dark Mode

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,18 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("Hello, world!")
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
         .padding(.horizontal)
         .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
     }
 }
 
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill()
+                .foregroundColor(.white)
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("✈️")
+                .font(.largeTitle)
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+         ContentView()
+            .preferredColorScheme(.dark)
         ContentView()
+            .preferredColorScheme(.light)
     }
 }
