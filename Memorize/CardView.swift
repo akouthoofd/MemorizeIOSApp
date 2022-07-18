da35e84029603a0c65b75b74e9335f9425b5111e
Alexander Kouthoofd <akouthoofd@salesforce.com>
2022-07-17 17:24:33 -0700

    Create Cardify generic card creator

--- Memorize/CardView.swift
+++ Memorize/CardView.swift
@@ -5,26 +5,25 @@
 //  Created by Alexander Kouthoofd on 7/10/22.
 //
 
+import Foundation
 import SwiftUI
 
 struct CardView: View {
     let card: EmojiMemoryGame.Card
     
     var body: some View {
-        GeometryReader(content: { geometry in
-            ZStack {
-                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-                if card.isFaceUp {
-                    shape.fill().foregroundColor(.white)
-                    shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
-                    Text(card.content).font(font(in: geometry.size))
-                } else if card.isMatched {
-                    shape.opacity(0)
-                } else {
-                    shape.fill()
-                }
+        GeometryReader { geometry in
+            let displayedCard = ZStack {
+                Text(card.content)
+                    .font(font(in: geometry.size))
             }
-        })
+            .cardify(isFaceUp: card.isFaceUp)
+            if (card.isMatched)  {
+                displayedCard.opacity(0)
+            } else {
+                displayedCard
+            }
+        }
     }
     
     private func font(in size: CGSize) -> Font {
@@ -32,8 +31,6 @@ struct CardView: View {
     }
     
     private struct DrawingConstants {
-        static let cornerRadius: CGFloat = 20
-        static let lineWidth: CGFloat = 3
         static let fontScale: CGFloat = 0.5
     }
 }
